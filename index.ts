c00fbc0cd7ab795e955da23c57f3862e0b6ed82a
Emma Nilsson <emma.nilsson@payfunc.com>
2021-02-03 12:56:20 +0100

    Exported client

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Authorization } from "./Authorization"
 import { Capture } from "./Capture"
+import { Client } from "./Client"
 import { Refund } from "./Refund"
 
-export { Authorization, Capture, Refund }
+export { Authorization, Capture, Client, Refund }
