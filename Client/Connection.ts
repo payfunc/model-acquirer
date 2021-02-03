bf9b7fef790197255ebfd5410cbd5757c4cc71aa
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2021-02-03 18:08:04 +0100

    Commonjs (#11)
    * Client class
    
    * commonjs module
    
    * Exported client
    
    * Updated package-lock
    
    * maintain commonjs as 0.0
    
    Co-authored-by: Emma Nilsson <emma.nilsson@payfunc.com>

---
+++ Client/Connection.ts
@@ -0,0 +1,40 @@
+import * as gracely from "gracely"
+import { default as fetch } from "isomorphic-fetch"
+
+export class Connection {
+	private constructor(readonly url: string, readonly token: string) {}
+
+	async post<Request, Response>(path: string, request: Request): Promise<Response | gracely.Error> {
+		const response = await fetch(this.url + "/" + path, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+				Authorization: "Bearer " + this.token,
+			},
+			body: JSON.stringify(request),
+		}).catch(_ => undefined)
+		return !response
+			? gracely.server.unavailable()
+			: response.headers.get("Content-Type")?.startsWith("application/json")
+			? response.json()
+			: response.text()
+	}
+	async remove<Response>(path: string): Promise<Response | gracely.Error> {
+		const response = await fetch(this.url + "/" + path, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json; charset=utf-8",
+				Authorization: "Bearer " + this.token,
+			},
+		}).catch(_ => undefined)
+
+		return !response
+			? gracely.server.unavailable()
+			: response.headers.get("Content-Type")?.startsWith("application/json")
+			? response.json()
+			: response.text()
+	}
+	static open(url: string | undefined, token: string | undefined): Connection | undefined {
+		return token && url ? new Connection(url, token) : undefined
+	}
+}
