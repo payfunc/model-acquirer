38506435f186a2b4fe375830c88c667f7ec9b9f0
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2021-02-19 16:41:40 +0100

    Added missing fields to Settlement.Transaction (#17)
    * Added missing fields to Settlement.Transaction
    
    Calculate Fee in model
    
    * isoly bump
    
    Co-authored-by: Robin Nedergård <robin.nedergard@payfunc.com>

--- Settlement/Transaction.ts
+++ Settlement/Transaction.ts
@@ -1,11 +1,15 @@
 import * as isoly from "isoly"
 import * as authly from "authly"
+import * as model from "@payfunc/model-card"
 import { Merchant } from "../Merchant"
 
 export interface Transaction {
 	authorization: authly.Identifier
 	reference: string
 	type: Merchant.Operation
+	card: "debit" | "credit"
+	scheme: model.Card.Scheme
+	area: isoly.CountryCode.Alpha2
 	created: isoly.Date
 	gross: number
 	fee: number
@@ -19,6 +23,9 @@ export namespace Transaction {
 			authly.Identifier.is(value.authorization) &&
 			typeof value.reference == "string" &&
 			Merchant.Operation.is(value.type) &&
+			(value.card == "debit" || value.card == "credit") &&
+			model.Card.Scheme.is(value.scheme) &&
+			isoly.CountryCode.Alpha2.is(value.area) &&
 			isoly.Date.is(value.created) &&
 			typeof value.gross == "number" &&
 			typeof value.fee == "number" &&
