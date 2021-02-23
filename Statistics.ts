5756dde1b3b87a2475bf89d7cd3f578688c8c09a
Emma Nilsson <emma.nilsson@payfunc.com>
2021-02-23 15:58:48 +0100

    Statistics interface.

--- Statistics.ts
+++ Statistics.ts
@@ -9,3 +9,20 @@ export interface Statistics {
 	fees: Record<isoly.Date, number>
 	refundable: number
 }
+
+export namespace Statistics {
+	export function is(value: any | Statistics): value is Statistics {
+		return (
+			typeof value == "object" &&
+			authly.Identifier.is(value.merchant) &&
+			isoly.Currency.is(value.currency) &&
+			typeof value.captured == "object" &&
+			Object.entries(value.captured).every(o => isoly.Date.is(o[0]) && typeof o[1] == "number") &&
+			typeof value.settled == "object" &&
+			Object.entries(value.settled).every(o => isoly.Date.is(o[0]) && typeof o[1] == "number") &&
+			typeof value.fees == "object" &&
+			Object.entries(value.fees).every(o => isoly.Date.is(o[0]) && typeof o[1] == "number") &&
+			typeof value.fees == "number"
+		)
+	}
+}
