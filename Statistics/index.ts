787fc79514c953f64dc791b2a7e20a835ea466d9
Emma Nilsson <31855183+nilssonemma@users.noreply.github.com>
2021-02-24 10:16:10 +0100

    Statistics (#24)
    * Initial
    
    * Transaction
    
    * Statistics interface.
    
    * Make history not optional
    
    * Sum function
    
    * Requested changes.
    
    * Added tests
    
    Co-authored-by: Florian Ebmeier <florian.ebmeier@payfunc.com>

---
+++ Statistics/index.ts
@@ -0,0 +1,39 @@
+import * as isoly from "isoly"
+import * as authly from "authly"
+
+export interface Statistics {
+	merchant: authly.Identifier
+	currency: isoly.Currency
+	captured: Record<isoly.Date, number>
+	settled: Record<isoly.Date, number>
+	fees: Record<isoly.Date, number>
+	refundable: number
+}
+
+export namespace Statistics {
+	export function is(value: any | Statistics): value is Statistics {
+		return (
+			typeof value == "object" &&
+			authly.Identifier.is(value.merchant, 8) &&
+			isoly.Currency.is(value.currency) &&
+			typeof value.captured == "object" &&
+			Object.entries(value.captured).every(e => isoly.Date.is(e[0]) && typeof e[1] == "number") &&
+			typeof value.settled == "object" &&
+			Object.entries(value.settled).every(e => isoly.Date.is(e[0]) && typeof e[1] == "number") &&
+			typeof value.fees == "object" &&
+			Object.entries(value.fees).every(e => isoly.Date.is(e[0]) && typeof e[1] == "number") &&
+			typeof value.fees == "number"
+		)
+	}
+	export function sum(record: Record<isoly.Date, number>, from?: isoly.Date | number, to?: isoly.Date): number {
+		if (typeof from == "number") {
+			const date = new Date()
+			date.setDate(date.getDate() - (from ?? 0))
+			from = isoly.Date.create(date)
+		}
+		return Object.entries(record).reduce(
+			(r, e) => ((!from || e[0] >= from) && e[0] <= (to ?? isoly.Date.now()) ? (r += e[1]) : r),
+			0
+		)
+	}
+}
