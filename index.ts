02470e9962c57c73625bd7bff112a083a8ae4b94
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-02-25 12:36:25 +0100

    Added Logs

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import { Authorization } from "./Authorization"
 import { Capture } from "./Capture"
 import { Client } from "./Client"
+import { Log } from "./Log"
 import { Merchant } from "./Merchant"
 import { Refund } from "./Refund"
 import { Settlement } from "./Settlement"
 import { Statistics } from "./Statistics"
 
-export { Authorization, Capture, Client, Merchant, Refund, Settlement, Statistics }
+export { Authorization, Capture, Client, Log, Merchant, Refund, Settlement, Statistics }
