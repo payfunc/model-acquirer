3369d2765d1402e99c9de0be4b7ed3f0ad0941d2
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2021-02-26 15:42:13 +0100

    Apply statistics on rules (#28)
    * Applying rules with statistics
    
    * Selectively bump

--- Merchant/Rules.spec.ts
+++ Merchant/Rules.spec.ts
@@ -1,26 +1,57 @@
 import * as model from "../index"
 
 describe("Rule", () => {
-	const rules: model.Merchant.Rules = ["reject authorization !has(verification)"]
+	const rules: model.Merchant.Rules = [
+		"reject authorization !has(verification)",
+		"reject authorization verification:(noServiceAvailable | rejected)",
+		"reject refund ($amount > $refundable)",
+	]
+	const captured = {
+		"2021-01-07": 100,
+		"2021-01-14": 150,
+	}
+	const settled = {
+		"2021-01-10": 95,
+	}
+	const fees = {
+		"2021-01-10": 5,
+	}
+	const statistic: model.Statistics = {
+		merchant: "testtest",
+		currency: "SEK",
+		captured,
+		settled,
+		fees,
+		refundable: model.Statistics.sum(captured) - model.Statistics.sum(settled),
+	}
 	it("is", () => {
 		expect(model.Merchant.Rules.is(rules)).toEqual(true)
 	})
 	it("parse", () => {
-		expect(model.Merchant.Rules.parse(rules).length).toEqual(1)
+		expect(model.Merchant.Rules.parse(rules).length).toEqual(3)
 	})
-	it("apply", () => {
-		expect(model.Merchant.Rules.apply({ verification: "someting" }, rules, "authorization")).toEqual(true)
-		expect(model.Merchant.Rules.apply({ notVerification: "someting" }, rules, "authorization")).toEqual({
-			type: "Rule Violation",
-			flaws: [{ type: "reject", condition: "!(has(verification))" }],
-		})
-		expect(model.Merchant.Rules.apply({ nested: { verification: "someting" } }, rules, "authorization")).toEqual(true)
-		expect(model.Merchant.Rules.apply({ nested: { notVerification: "someting" } }, rules, "authorization")).toEqual({
-			type: "Rule Violation",
-			flaws: [{ type: "reject", condition: "!(has(verification))" }],
-		})
+	it("apply authorization", () => {
+		expect(model.Merchant.Rules.apply({ statistic, verification: "verified" }, rules, "authorization")).toEqual(true)
 		expect(
-			model.Merchant.Rules.apply({ nested: { verification: {  } } }, rules, "authorization")
-		).toEqual(true)
+			model.Merchant.Rules.apply({ statistic, verification: "noServiceAvailable" }, rules, "authorization")
+		).toEqual({
+			type: "Rule Violation",
+			flaws: [{ type: "reject", condition: "verification:(noServiceAvailable | rejected)" }],
+		})
+		expect(model.Merchant.Rules.apply({ statistic, verification: "rejected" }, rules, "authorization")).toEqual({
+			type: "Rule Violation",
+			flaws: [{ type: "reject", condition: "verification:(noServiceAvailable | rejected)" }],
+		})
+		expect(model.Merchant.Rules.apply({ statistic }, rules, "authorization")).toEqual({
+			type: "Rule Violation",
+			flaws: [{ type: "reject", condition: "!(has(verification))" }],
+		})
+	})
+	it("apply refund", () => {
+		expect(model.Merchant.Rules.apply({ refund: { amount: 30 }, statistic }, rules, "refund")).toEqual(true)
+		expect(model.Merchant.Rules.apply({ refund: { amount: 300 }, statistic }, rules, "refund")).toEqual({
+			type: "Rule Violation",
+			flaws: [{ type: "reject", condition: "($amount > $refundable)" }],
+		})
 	})
 })
