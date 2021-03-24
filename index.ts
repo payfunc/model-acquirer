64801c75fbcf840a2ddb62cf98df2331cc545d43
Robin Nedergård <robin.nedergard@payfunc.com>
2021-03-24 12:59:52 +0100

    Fixed export and test with isoly.Date.now()

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { Client } from "./Client"
 import { Merchant } from "./Merchant"
 import { Refund } from "./Refund"
 import { Settlement } from "./Settlement"
+import * as State from "./State"
 import { Statistics } from "./Statistics"
 
-export { Authorization, Capture, Client, Merchant, Refund, Settlement, Statistics }
+export { Authorization, Capture, Client, Merchant, Refund, Settlement, State, Statistics }
