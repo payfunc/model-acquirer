154a91872ef784091e61d6dbe16840d805472a6b
Robin Nedergård <robin.nedergard@payfunc.com>
2021-03-31 18:22:59 +0200

    Tests and mini fixes

--- State/FailedAuthorization.spec.ts
+++ State/FailedAuthorization.spec.ts
@@ -54,4 +54,266 @@ describe("State.FailedAuthorization tests", () => {
 		}
 		expect(acquirer.State.FailedAuthorization.is(lateFailure)).toBeTruthy()
 	})
+	it("load", () => {
+		expect(
+			acquirer.State.FailedAuthorization.load(
+				[
+					{
+						id: "1234567890000000",
+						number: "1234567890123456",
+						created: "2021-02-01T23:59:59.000Z",
+						currency: "SEK",
+						history: [],
+						reference: "",
+						refund: [],
+						capture: [],
+						card: { iin: "123456", last4: "1234", expires: [3, 24], csc: "present", scheme: "visa", type: "debit" },
+						amount: 100,
+					},
+				],
+				[
+					{
+						agent: "PayFunc",
+						reference: { id: "1234567890abcdef", number: "1234567890123455", type: "authorization" },
+						merchant: "testtest",
+						client: "",
+						resource: { method: "GET", location: "http://example.com" },
+						created: "2021-02-02T23:59:59.000Z",
+						entries: [
+							{
+								level: "trace",
+								point: "request",
+								data: {
+									headers: {
+										accept: ["*/*"],
+										acceptEncoding: ["gzip"],
+										authorization: "Bearer key.key.key",
+										connection: "Keep-Alive",
+										contentLength: "233",
+										contentType: "application/json",
+										host: "worker-authorization-development.authtest.workers.dev",
+										userAgent: "PostmanRuntime/7.26.8",
+										xForwardedProto: "https",
+									},
+									url: "https://worker-authorization-development.authtest.workers.dev/authorization",
+								},
+							},
+							{
+								level: "trace",
+								point: "Authorization Data before Typeguards",
+								data: {
+									amount: 15,
+									currency: "EUR",
+								},
+							},
+							{
+								level: "log",
+								point: "PreAuthorization State",
+								data: {
+									state: {
+										merchant: {
+											descriptor: "Test Merchant",
+											country: "SE",
+											name: "Test Merchant",
+											currency: "SEK",
+											scheme: [],
+											refundable: 9.66,
+											captured: 10,
+											settled: 0.24000000000000002,
+											fees: 0.1,
+										},
+										authorization: {
+											amount: 15,
+											currency: "EUR",
+											card: {
+												scheme: "mastercard",
+												iin: "520474",
+												last4: "1002",
+												expires: "2022-02-28",
+												csc: "present",
+											},
+											descriptor: "string",
+										},
+										now: "2021-03-31",
+									},
+								},
+							},
+							{
+								level: "log",
+								point: "Ipg Response",
+								data: {
+									response: {
+										clientRequestId: "83332605",
+										apiTraceId: "rrt-01d417a497a71fc2d-a-de-16809-101275597-1",
+										responseType: "GatewayDeclined",
+										orderId: "uj0_7HzydkbNh09O",
+										transactionTime: 1617189739,
+										transactionStatus: "VALIDATION_FAILED",
+										error: {
+											code: "100",
+											message: "Internal error",
+										},
+									},
+								},
+							},
+							{
+								level: "warning",
+								point: "response",
+								data: {
+									status: 502,
+									header: {
+										contentType: "application/json; charset=utf-8",
+										accessControlAllowOrigin: "*",
+									},
+									body: {
+										status: 502,
+										type: "backend failure",
+										backend: "unknown",
+										details: {
+											code: "100",
+											message: "Internal error",
+										},
+									},
+								},
+							},
+						],
+					},
+				]
+			)
+		).toEqual([
+			{
+				authorization: {
+					amount: 15,
+					card: {
+						csc: "present",
+						expires: "2022-02-28",
+						iin: "520474",
+						last4: "1002",
+						scheme: "mastercard",
+					},
+					currency: "EUR",
+					descriptor: "string",
+					number: "1234567890123455",
+				},
+				created: "2021-02-02T23:59:59.000Z",
+				log: [
+					{
+						agent: "PayFunc",
+						client: "",
+						created: "2021-02-02T23:59:59.000Z",
+						entries: [
+							{
+								data: {
+									headers: {
+										accept: ["*/*"],
+										acceptEncoding: ["gzip"],
+										authorization: "Bearer key.key.key",
+										connection: "Keep-Alive",
+										contentLength: "233",
+										contentType: "application/json",
+										host: "worker-authorization-development.authtest.workers.dev",
+										userAgent: "PostmanRuntime/7.26.8",
+										xForwardedProto: "https",
+									},
+									url: "https://worker-authorization-development.authtest.workers.dev/authorization",
+								},
+								level: "trace",
+								point: "request",
+							},
+							{
+								data: {
+									amount: 15,
+									currency: "EUR",
+								},
+								level: "trace",
+								point: "Authorization Data before Typeguards",
+							},
+							{
+								data: {
+									state: {
+										authorization: {
+											amount: 15,
+											card: {
+												csc: "present",
+												expires: "2022-02-28",
+												iin: "520474",
+												last4: "1002",
+												scheme: "mastercard",
+											},
+											currency: "EUR",
+											descriptor: "string",
+										},
+										merchant: {
+											captured: 10,
+											country: "SE",
+											currency: "SEK",
+											descriptor: "Test Merchant",
+											fees: 0.1,
+											name: "Test Merchant",
+											refundable: 9.66,
+											scheme: [],
+											settled: 0.24000000000000002,
+										},
+										now: "2021-03-31",
+									},
+								},
+								level: "log",
+								point: "PreAuthorization State",
+							},
+							{
+								data: {
+									response: {
+										apiTraceId: "rrt-01d417a497a71fc2d-a-de-16809-101275597-1",
+										clientRequestId: "83332605",
+										error: {
+											code: "100",
+											message: "Internal error",
+										},
+										orderId: "uj0_7HzydkbNh09O",
+										responseType: "GatewayDeclined",
+										transactionStatus: "VALIDATION_FAILED",
+										transactionTime: 1617189739,
+									},
+								},
+								level: "log",
+								point: "Ipg Response",
+							},
+							{
+								data: {
+									body: {
+										backend: "unknown",
+										details: {
+											code: "100",
+											message: "Internal error",
+										},
+										status: 502,
+										type: "backend failure",
+									},
+									header: {
+										accessControlAllowOrigin: "*",
+										contentType: "application/json; charset=utf-8",
+									},
+									status: 502,
+								},
+								level: "warning",
+								point: "response",
+							},
+						],
+						merchant: "testtest",
+						reference: {
+							id: "1234567890abcdef",
+							number: "1234567890123455",
+							type: "authorization",
+						},
+						resource: {
+							location: "http://example.com",
+							method: "GET",
+						},
+					},
+				],
+				merchant: "testtest",
+				reason: "Internal error",
+			},
+		])
+	})
 })
