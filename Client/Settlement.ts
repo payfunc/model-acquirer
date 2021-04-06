2e6ebe139a7d5411230f6507213f1a71290d54bf
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-04-06 14:29:03 +0200

    added listing with query

--- Client/Settlement.ts
+++ Client/Settlement.ts
@@ -5,15 +5,19 @@ import { Connection } from "./Connection"
 
 export class Settlement {
 	constructor(readonly client: { connection: Connection }) {}
-	async load(): Promise<ModelSettlement[] | gracely.Error>
-	async load(id?: authly.Identifier): Promise<ModelSettlement | gracely.Error>
-	async load(id?: authly.Identifier[]): Promise<ModelSettlement[] | gracely.Error>
+	async list(date?: string, days?: number): Promise<ModelSettlement[] | gracely.Error> {
+		return await this.client.connection.get<ModelSettlement[]>(
+			"settlement" +
+				(date && days ? `?date=${date}&days=${days}` : date ? `?date=${date}` : days ? `?days=${days}` : "")
+		)
+	}
+
+	async load(id: authly.Identifier): Promise<ModelSettlement | gracely.Error>
+	async load(id: authly.Identifier[]): Promise<ModelSettlement[] | gracely.Error>
 	async load(
-		id?: authly.Identifier | authly.Identifier[]
+		id: authly.Identifier | authly.Identifier[]
 	): Promise<ModelSettlement | ModelSettlement[] | gracely.Error> {
-		return !id
-			? await this.client.connection.get<ModelSettlement[]>("settlement")
-			: authly.Identifier.is(id)
+		return authly.Identifier.is(id)
 			? await this.client.connection.get<ModelSettlement>("settlement/" + id)
 			: (await Promise.all(id.map(i => this.client.connection.get<ModelSettlement>("settlement/" + i)))).filter(
 					ModelSettlement.is
