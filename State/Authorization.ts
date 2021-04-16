4f46aae212676d5f5a16ca910bb71ee1e1db033d
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-04-16 13:06:15 +0200

    Updated typeguard

--- State/Authorization.ts
+++ State/Authorization.ts
@@ -32,24 +32,26 @@ export namespace Authorization {
 			(Merchant.is(value.merchant) || (typeof value.merchant == "object" && authly.Identifier.is(value.merchant.id))) &&
 			typeof value.authorization == "object" &&
 			typeof value.authorization.id == "string" &&
+			(value.authorization.number == undefined || typeof value.authorization.number == "string") &&
+			typeof value.authorization.reference == "string" &&
+			isoly.DateTime.is(value.authorization.created) &&
 			typeof value.authorization.amount == "number" &&
 			isoly.Currency.is(value.authorization.currency) &&
 			Card.is(value.authorization.card) &&
-			isoly.DateTime.is(value.authorization.created) &&
 			(value.authorization.descriptor == undefined || typeof value.authorization.descriptor == "string") &&
-			(value.authorization.number == undefined || typeof value.authorization.number == "string") &&
-			(value.authorization.reference == undefined || typeof value.authorization.reference == "string") &&
-			(value.authorization.captured == undefined ||
-				(typeof value.authorization.captured == "object" &&
-					typeof value.authorization.captured.amount == "number" &&
-					isoly.DateTime.is(value.authorization.captured.latest))) &&
-			(value.authorization.refunded == undefined ||
-				(typeof value.authorization.refunded == "object" &&
-					typeof value.authorization.refunded.amount == "number" &&
-					isoly.DateTime.is(value.authorization.refunded.latest))) &&
-			(value.authorization.voided == undefined || isoly.DateTime.is(value.authorization.voided)) &&
 			(value.authorization.recurring == undefined ||
 				["initial", "subsequent"].includes(value.authorization.recurring)) &&
+			Array.isArray(value.authorization.history) &&
+			value.authorization.history.every(acquirer.Authorization.Change.is) &&
+			Array.isArray(value.authorization.capture) &&
+			value.authorization.capture.every(acquirer.Capture.is) &&
+			Array.isArray(value.authorization.refund) &&
+			value.authorization.refund.every(acquirer.Refund.is) &&
+			(value.authorization.voided == undefined || isoly.DateTime.is(value.authorization.voided)) &&
+			typeof value.authorization.status == "object" &&
+			Object.entries(value.authorization.status).every(
+				entry => acquirer.Authorization.Status.is(entry[0]) && typeof entry[1] == "string"
+			) &&
 			isoly.DateTime.is(value.created)
 		)
 	}
