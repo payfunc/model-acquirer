12f8cab797b1db1c4e3aac323b76a63132ef5aca
Emma Nilsson <emma.nilsson@payfunc.com>
2021-04-26 15:52:44 +0200

    Added scheme fee to settlement

--- Settlement/index.spec.ts
+++ Settlement/index.spec.ts
@@ -12,7 +12,7 @@ describe("Settlement", () => {
 			payout: "2020-03-02",
 			created: "2020-01-16",
 			gross: 2,
-			fee: 3,
+			fee: { scheme: 2, total: 3 },
 			net: 4,
 			currency: "EUR",
 			transactions: [
@@ -59,7 +59,7 @@ describe("Settlement", () => {
 			payout: "2020-03-02",
 			created: "2020-01-16",
 			gross: 2,
-			fee: 3,
+			fee: { scheme: 2, total: 3 },
 			net: 4,
 			currency: "EUR",
 			transactions: [],
@@ -74,7 +74,7 @@ describe("Settlement", () => {
 			payout: "2020-03-02",
 			created: "2020-01-16",
 			gross: 2,
-			fee: 3,
+			fee: { scheme: 2, total: 6 },
 			net: 4,
 			currency: "EUR",
 			transactions: [],
@@ -130,9 +130,106 @@ describe("Settlement", () => {
 		expect(csv).toEqual(
 			"reference,merchant,start date,end date,payout date,reserve amount,reserve payout,created,gross,fee,net,currency\r\n" +
 				'"example1","SQJzzNur","2020-02-01","2020-02-07","2020-03-02","undefined","undefined","2020-01-16","2","3","4","EUR"\r\n' +
-				'"example2","SQJzzNur","2020-02-01","2020-02-07","2020-03-02","undefined","undefined","2020-01-16","2","3","4","EUR"\r\n' +
+				'"example2","SQJzzNur","2020-02-01","2020-02-07","2020-03-02","undefined","undefined","2020-01-16","2","6","4","EUR"\r\n' +
 				'"example3","SQJzzNur","2020-02-01","2020-02-07","2020-03-02","undefined","undefined","2020-01-16","2","3","4","EUR"\r\n' +
 				'"noPayout","SQJzzNur","2020-02-01","2020-02-07","undefined","undefined","undefined","2020-01-16","2","3","4","EUR"\r\n'
 		)
+	})
+	it("toCustomer", () => {
+		const result = Settlement.toCustomer(settlement)
+		expect(result).toEqual([
+			{
+				reference: "example",
+				merchant: "SQJzzNur",
+				period: {
+					start: "2020-02-01",
+					end: "2020-02-07",
+				},
+				payout: "2020-03-02",
+				created: "2020-01-16",
+				gross: 2,
+				fee: 3,
+				net: 4,
+				currency: "EUR",
+				transactions: [
+					{
+						authorization: "12345",
+						reference: "234242",
+						type: "authorization",
+						card: "debit",
+						scheme: "mastercard",
+						area: "SE",
+						created: "2020-02-16",
+						gross: 2,
+						fee: 3,
+						net: 12,
+					},
+				],
+			},
+		])
+	})
+	it("toCustomer with multiple settlements", () => {
+		const result = Settlement.toCustomer(multiple)
+		expect(result).toEqual([
+			{
+				reference: "example1",
+				merchant: "SQJzzNur",
+				period: {
+					start: "2020-02-01",
+					end: "2020-02-07",
+				},
+				payout: "2020-03-02",
+				created: "2020-01-16",
+				gross: 2,
+				fee: 3,
+				net: 4,
+				currency: "EUR",
+				transactions: [],
+			},
+			{
+				reference: "example2",
+				merchant: "SQJzzNur",
+				period: {
+					start: "2020-02-01",
+					end: "2020-02-07",
+				},
+				payout: "2020-03-02",
+				created: "2020-01-16",
+				gross: 2,
+				fee: 6,
+				net: 4,
+				currency: "EUR",
+				transactions: [],
+			},
+			{
+				reference: "example3",
+				merchant: "SQJzzNur",
+				period: {
+					start: "2020-02-01",
+					end: "2020-02-07",
+				},
+				payout: "2020-03-02",
+				created: "2020-01-16",
+				gross: 2,
+				fee: 3,
+				net: 4,
+				currency: "EUR",
+				transactions: [],
+			},
+			{
+				reference: "noPayout",
+				merchant: "SQJzzNur",
+				period: {
+					start: "2020-02-01",
+					end: "2020-02-07",
+				},
+				created: "2020-01-16",
+				gross: 2,
+				fee: 3,
+				net: 4,
+				currency: "EUR",
+				transactions: [],
+			},
+		])
 	})
 })
--- Settlement/index.ts
+++ Settlement/index.ts
@@ -13,7 +13,7 @@ export interface Settlement {
 	reserve?: { amount: number; payout?: isoly.Date }
 	created: isoly.Date
 	gross: number
-	fee: number
+	fee: number | { scheme: number; total: number }
 	net: number
 	currency: isoly.Currency
 	transactions: SettlementTransaction[]
@@ -34,7 +34,7 @@ export namespace Settlement {
 			(value.reserve.payout == undefined || isoly.Date.is(value.reserve.payout)) &&
 			isoly.Date.is(value.created) &&
 			typeof value.gross == "number" &&
-			typeof value.fee == "number" &&
+			(typeof value.fee == "number" || (typeof value.fee.scheme == "number" && typeof value.fee.total)) &&
 			typeof value.net == "number" &&
 			isoly.Currency.is(value.currency) &&
 			Array.isArray(value.transactions) &&
@@ -50,9 +50,19 @@ export namespace Settlement {
 		let result =
 			"reference,merchant,start date,end date,payout date,reserve amount,reserve payout,created,gross,fee,net,currency\r\n"
 		for (const value of settlements) {
-			result += `"${value.reference}","${value.merchant}","${value.period.start}","${value.period.end}","${value.payout}","${value.reserve?.amount}","${value.reserve?.payout}","${value.created}","${value.gross}","${value.fee}","${value.net}","${value.currency}"\r\n`
+			result += `"${value.reference}","${value.merchant}","${value.period.start}","${value.period.end}","${
+				value.payout
+			}","${value.reserve?.amount}","${value.reserve?.payout}","${value.created}","${value.gross}","${
+				typeof value.fee == "number" ? value.fee : value.fee.total
+			}","${value.net}","${value.currency}"\r\n`
 			result += value.transactions.length > 0 ? SettlementTransaction.toCsv(value.transactions) : ""
 		}
 		return result
+	}
+	export function toCustomer(settlements: Settlement[]): Settlement[] {
+		return settlements.reduce<Settlement[]>(
+			(r, s) => (r = [...r, { ...s, fee: typeof s.fee == "object" ? s.fee.total : s.fee }]),
+			[]
+		)
 	}
 }
