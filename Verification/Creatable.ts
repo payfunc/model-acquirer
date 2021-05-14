ed0e07686e4078455696e230fa7036a95137b6fe
Florian Ebmeier <florian.ebmeier@payfunc.com>
2021-05-14 16:36:33 +0200

    Added Verification Creatable flaw

--- Verification/Creatable.ts
+++ Verification/Creatable.ts
@@ -1,3 +1,4 @@
+import * as gracely from "gracely"
 import * as isoly from "isoly"
 import * as authly from "authly"
 import * as model from "@payfunc/model-card"
@@ -33,5 +34,41 @@ export namespace Creatable {
 			(value.customer == undefined || base.Customer.is(value.customer)) &&
 			typeof value.target == "string"
 		)
+	}
+	export function flaw(value: Creatable | any): gracely.Flaw {
+		return {
+			type: "Verification.Creatable",
+			flaws:
+				typeof value != "object"
+					? undefined
+					: ([
+							typeof value.number == "string" || { property: "number", type: "string" },
+							typeof value.items == "number" ||
+								base.Item.is(value.items) ||
+								(Array.isArray(value.items) && value.items.every(base.Item.is)) || {
+									property: "items",
+									type: "number | Item | Item[]",
+								},
+							value.response == undefined ||
+								(typeof value.response == "object" &&
+									["method", "challenge", "pares"].includes(value.response.type) &&
+									typeof value.response.data == "string") || {
+									property: "response",
+									type: '{type:"pares" | "method" | "challenge";data:string}',
+								},
+							value.browser == undefined ||
+								base.Browser.is(value.browser) || { property: "browser", type: "base.Browser" },
+							isoly.Currency.is(value.currency) || { property: "currency", type: "isoly.Currency" },
+							authly.Token.is(value.card) ||
+								model.Card.Creatable.is(value.card) || { property: "card", type: "Card.Creatable | authly.Token" },
+							value.recurring == undefined ||
+								["initial", "subsequent"].includes(value.recurring) || {
+									property: "recurring",
+									type: '"initial" | "subsequent" | undefined',
+								},
+							value.customer == undefined || base.Customer.is(value.customer) || base.Customer.flaw(value.customer),
+							typeof value.target == "string" || { property: "target", type: "string" },
+					  ].filter(gracely.Flaw.is) as gracely.Flaw[]),
+		}
 	}
 }
