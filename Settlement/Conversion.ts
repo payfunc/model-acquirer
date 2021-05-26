6a0588369bb283457a7eccd66cb7402f1a9eeb3f
Marcus Hanses <marcus.hanses@intergiro.com>
2021-05-26 15:53:52 +0200

    Requested change.

--- Settlement/Conversion.ts
+++ Settlement/Conversion.ts
@@ -4,19 +4,40 @@ import { Settlement } from "./Settlement"
 
 export namespace Conversion {
 	export function toCsv(settlements: Settlement[]): string {
-		let result =
-			"reference,merchant,start,end,payout date,payout amount,reserve release,reserve amount,created,gross,fee,interchange,net,currency,transactions\r\n"
-		for (const settlement of settlements)
-			result += `"${settlement.reference}","${settlement.merchant}","${settlement.period.start}","${
-				settlement.period.end
-			}","${settlement.payout}","${isoly.Currency.round(
-				settlement.net - (settlement.reserve?.amount ?? 0),
-				settlement.currency
-			)}","${settlement.reserve?.payout ?? ""}","${settlement.reserve?.amount ?? 0}","${settlement.created}","${
-				settlement.gross
-			}","${typeof settlement.fee == "number" ? settlement.fee : settlement.fee.total}","${
-				typeof settlement.fee == "number" ? 0 : settlement.fee.scheme
-			}","${settlement.net}","${settlement.currency}","${settlement.transactions.length}"\r\n`
+		let result = ""
+		result += "reference,"
+		result += "merchant,"
+		result += "start,"
+		result += "end,"
+		result += "payout date,"
+		result += "payout amount,"
+		result += "reserve release,"
+		result += "reserve amount,"
+		result += "created,"
+		result += "gross,"
+		result += "fee,"
+		result += "interchange,"
+		result += "net,"
+		result += "currency,"
+		result += "transactions\r\n"
+		for (const settlement of settlements) {
+			result += `"${settlement.reference}",`
+			result += `"${settlement.merchant}",`
+			result += `"${settlement.period.start}",`
+			result += `"${settlement.period.end}",`
+			result += `"${settlement.payout}",`
+			result += `"${isoly.Currency.round(settlement.net - (settlement.reserve?.amount ?? 0), settlement.currency)}",`
+			result += `"${settlement.reserve?.payout ?? ""}",`
+			result += `"${settlement.reserve?.amount ?? 0}",`
+			result += `"${settlement.created}",`
+			result += `"${settlement.gross}",`
+			result += `"${typeof settlement.fee == "number" ? settlement.fee : settlement.fee.total}",`
+			result += `"${typeof settlement.fee == "number" ? 0 : settlement.fee.scheme}",`
+			result += `"${settlement.net}",`
+			result += `"${settlement.currency}",`
+			result += `"${settlement.transactions.length}"`
+			result += `\r\n`
+		}
 		return result
 	}
 	export function toDetailedCsv(settlements: Settlement[], authorizations: Authorization[]): string {
@@ -27,19 +48,16 @@ export namespace Conversion {
 		result += "posting date,"
 		result += "authorization id,"
 		result += "authorization number,"
-
 		result += "card IIN,"
 		result += "card type,"
 		result += "card scheme,"
 		result += "scheme reference,"
 		result += "issuer country,"
-
 		result += "currency,"
 		result += "payout date,"
 		result += "payout amount,"
 		result += "reserve release,"
 		result += "reserve amount,"
-
 		result += "gross,"
 		result += "fee,"
 		result += "interchange,"
@@ -54,13 +72,11 @@ export namespace Conversion {
 				result += `"${transaction.created}",`
 				result += `"${transaction.authorization}",`
 				result += `"${authorization?.number ?? ""}",`
-
 				result += `"${authorization?.card.iin ?? ""}",`
 				result += `"${transaction.card}",`
 				result += `"${transaction.scheme}",`
 				result += `"${transaction.reference}",`
 				result += `"${transaction.area}",`
-
 				result += `"${transaction.currency}",`
 				result += `"${settlement?.payout ?? ""}",`
 				result += `"${isoly.Currency.round(
@@ -69,12 +85,10 @@ export namespace Conversion {
 				)}",`
 				result += `"${transaction.reserve?.payout ?? ""}",`
 				result += `"${transaction.reserve?.amount ?? 0}",`
-
 				result += `"${transaction.gross}",`
 				result += `"${typeof transaction.fee == "number" ? transaction.fee : transaction.fee.total}",`
 				result += `"${typeof transaction.fee == "number" ? 0 : transaction.fee.scheme}",`
 				result += `"${transaction.net}"`
-
 				result += `\r\n`
 			}
 		}
