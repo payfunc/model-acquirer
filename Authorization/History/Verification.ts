be4185bf54c41ea53ccabaa1148a3441af58ad79
Marcus Hanses <marcus.hanses@intergiro.com>
2021-06-22 11:17:07 +0200

    Renamed forgotten customer to contact.

--- Authorization/History/Verification.ts
+++ Authorization/History/Verification.ts
@@ -21,7 +21,8 @@ interface Base {
 	currency: isoly.Currency
 	target: string
 	browser?: base.Browser
-	customer?: base.Contact
+	customer?: base.Contact // @deprecated
+	contact?: base.Contact
 	recurring?: Recurring
 }
 
@@ -60,6 +61,7 @@ export namespace Verification {
 			typeof value.target == "string" &&
 			(value.browser == undefined || base.Browser.is(value.browser)) &&
 			(value.customer == undefined || base.Contact.is(value.customer)) &&
+			(value.contact == undefined || base.Contact.is(value.contact)) &&
 			(value.recurring == undefined || Recurring.is(value.recurring))
 		)
 	}
@@ -108,7 +110,7 @@ export namespace Verification {
 			currency: creatable.currency,
 			target: creatable.target,
 			browser: creatable.browser,
-			customer: creatable.customer,
+			contact: creatable.contact,
 			recurring: creatable.recurring,
 			response,
 			...(!gracely.Error.is(input)
